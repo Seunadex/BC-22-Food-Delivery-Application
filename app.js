3b2c6820626a1bb39b7993c90ff8f9ba6a404b4a
Adekunle Seun <adekunleseun001@gmail.com>
2017-05-15 21:45:02 +0100

    Add server file

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const auth = require('./controllers/auth.js');
+
+
+// Initialize express
+const app = express();
+
+
+
+//home
+app.get('/', (req, res) =>{
+	res.render("index", {error: null});
+});
+
+// Register page
+app.get('/register', (req, res) => {
+	res.render('register', {error: null});
+});
+
+// Set up template engine
+app.set('views', process.cwd() + '/views');
+app.set('view engine', 'ejs');
+
+// Statisc files
+app.use(express.static('assets'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// getting post data
+app.post('/register', auth.register);
+app.post('/', auth.login);
+app.get('/signOut', auth.signOut);
+app.get('/google', auth.google);
+
+app.use('/dashboard', (req, res, next) => {
+	next()
+}, require('./routes/dashboard.js'));
+
+
+// fire controllers
+
+
+// Listen to a port
+app.listen(3000, ()=> {
+	console.log('You are listening to port 3000')
+});
